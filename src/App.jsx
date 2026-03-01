e5da77580b853f7e3b1f226e4daa0c31e5db4522
Ritam Chakraborty <chakrabortyritam29@gmail.com>
2026-03-02 01:29:25 +0530

    Created the Navbar, Hero Section, Service section, Service card section, Tittle section, Trustedby(For now, will change in future) section, ThemeToggleButton Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
+import ServiceCard from './components/ServiceCard'
 
 const App = () => {
+
+  const [theme,setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
   return (
-    <div>
-      
+    <div className='dark:bg-black relative'>
+    <Navbar theme={theme} setTheme={setTheme} /> 
+    <Hero />
+    <TrustedBy />
+    <Services />
+    
     </div>
   )
 }
